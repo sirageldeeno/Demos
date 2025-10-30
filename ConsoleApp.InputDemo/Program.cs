41c710699bb61e49d08f61957578ca70882a321e
ASER <ASER@DESKTOP-03CVPQ3>
2025-10-30 18:53:03 +0400

    Add project files.

---
+++ ConsoleApp.InputDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp.InputDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            string name = string.Empty;
+            int age = 0;
+            int retirmentAge = 62;
+            int retirmentLeftYears = 0;
+            //Prompt user
+            Console.WriteLine("Enter your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age : ");
+            age = Convert.ToInt32(Console.ReadLine());
+            //process reirment left years
+            retirmentLeftYears = retirmentAge - age;
+            //Output data
+            Console.WriteLine($"Your Name is : {name}");
+            Console.WriteLine($"Your Age is : {age}");
+            Console.WriteLine($"Your RetirmentLeft Years : {retirmentLeftYears}");
+
+
+
+
+
+
+        }
+    }
+}
---
+++ DemoOutput/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is my first coding");
+        }
+    }
+}
