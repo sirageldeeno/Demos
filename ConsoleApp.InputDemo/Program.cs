e367b921704ed22f92de8889f6ef3404cac1f15a
ASER <ASER@DESKTOP-03CVPQ3>
2025-10-30 19:15:19 +0400

    update to firstname and last name

--- ConsoleApp.InputDemo/Program.cs
+++ ConsoleApp.InputDemo/Program.cs
@@ -1,23 +1,29 @@
-﻿namespace ConsoleApp.InputDemo
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp.InputDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Declare variables
-            string name = string.Empty;
+            string firstName = string.Empty;
+            string lastName = string.Empty;
+
             int age = 0;
             int retirmentAge = 62;
             int retirmentLeftYears = 0;
             //Prompt user
-            Console.WriteLine("Enter your name : ");
-            name = Console.ReadLine();
+            Console.WriteLine("Enter your first name : ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name : ");
+            lastName = Console.ReadLine();
             Console.WriteLine("Enter your age : ");
             age = Convert.ToInt32(Console.ReadLine());
             //process reirment left years
             retirmentLeftYears = retirmentAge - age;
             //Output data
-            Console.WriteLine($"Your Name is : {name}");
+            Console.WriteLine($"Your full Name is : {firstName} {lastName}");
             Console.WriteLine($"Your Age is : {age}");
             Console.WriteLine($"Your RetirmentLeft Years : {retirmentLeftYears}");
 
