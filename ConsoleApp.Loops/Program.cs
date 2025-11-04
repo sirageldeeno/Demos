4fb7f76fae15b986950dfe815240209d463196f4
ASER <ASER@DESKTOP-03CVPQ3>
2025-11-04 17:38:56 +0400

    refacting calculator with loops

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("How many times would you like to print Hello World");
+int times= Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < times; i++)
+{
+    Console.WriteLine("Hello World");
+}
+
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,48 +1,73 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //sample calculator program
 // Declare variables
-int result=0;
+
 // Welcome Message
 Console.WriteLine("Welcome To Calculator Program");
-// prompt user to Enter First number and Second number
-Console.Write("Enter First Number: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter Second Number: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-// Show user Options
+Console.WriteLine("-------------------------------");
+Console.WriteLine("Choose operation or (-1) To Exit");
 Console.WriteLine("Please Select an operation");
 Console.WriteLine("1- Adition");
 Console.WriteLine("2- Substration");
 Console.WriteLine("3- Production");
 Console.WriteLine("4- Division");
-// Read Operation
-int operationNumber= Convert.ToInt32(Console.ReadLine());
-// process calculation
-switch (operationNumber)
+Console.WriteLine("5- Fibonachie");
+int operationNumber = Convert.ToInt32(Console.ReadLine());
+while (operationNumber != -1)
 {
-    case 1:
-        result = firstNumber + secondNumber;
-      
-        break;
-    case 2:
-        result = firstNumber - secondNumber;
-        
-        break;
-    case 3:
-        result = firstNumber * secondNumber;
-       
-        break;
-    case 4:
-        result = firstNumber / secondNumber;
-        
-        break;
-    default:
-        Console.WriteLine("Wrong Operation");
+    Console.Write("Enter First Number: ");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter Second Number: ");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+    int result = 0;
+    // process calculation
+    switch (operationNumber)
+    {
+        case 1:
+            result = firstNumber + secondNumber;
 
-        break;
+            break;
+        case 2:
+            result = firstNumber - secondNumber;
 
-}
-Console.WriteLine($"Answer is : {result} ");
+            break;
+        case 3:
+            result = firstNumber * secondNumber;
+
+            break;
+        case 4:
+            result = firstNumber / secondNumber;
+
+            break;
+        case 5:
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                result += i;
+            }
+
+            break;
+        default:
+            Console.WriteLine("Wrong Operation");
+
+            break;
+
+    }
+    Console.WriteLine($"Answer is : {result} ");
+    Console.WriteLine("Press Enter To Continue");
+    Console.ReadLine();
+    Console.WriteLine("Choose operation or (-1) To Exit");
+    Console.WriteLine("Please Select an operation");
+    Console.WriteLine("1- Adition");
+    Console.WriteLine("2- Substration");
+    Console.WriteLine("3- Production");
+    Console.WriteLine("4- Division");
+    Console.WriteLine("5- Fibonachie");
+     operationNumber= Convert.ToInt32(Console.ReadLine());
+
+
+
+}//End of while
+    Console.WriteLine("Thank you  for using program");
 
 
 
